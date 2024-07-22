e8bc68462583198dd6b81f68b580f878a19ad14c
Haroon <haroon5507@gmail.com>
2024-07-22 14:40:49 +0500

    secondcommit

---
+++ SDKmedIQ/SDKmedIQ/ReceiverMedIQ.swift
@@ -0,0 +1,22 @@
+//
+//  ReceiverMedIQ.swift
+//  SDKmedIQ
+//
+//  Created by Haroon Shoukat on 22/07/2024.
+//
+
+import Foundation
+
+public class ReceiverMedIQ{
+    public init(){}
+    
+    public func configSDK(){
+        print("@@--- Test SDK")
+        print("@@--00000t=")
+    }
+    
+    
+    public func getPharmacies(){
+        print("@@@@2------------")
+    }
+}
---
+++ SDKmedIQ/SDKmedIQ/SDKmedIQ.h
@@ -0,0 +1,18 @@
+//
+//  SDKmedIQ.h
+//  SDKmedIQ
+//
+//  Created by Haroon Shoukat on 22/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKmedIQ.
+FOUNDATION_EXPORT double SDKmedIQVersionNumber;
+
+//! Project version string for SDKmedIQ.
+FOUNDATION_EXPORT const unsigned char SDKmedIQVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKmedIQ/PublicHeader.h>
+
+
